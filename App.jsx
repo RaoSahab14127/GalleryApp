6bbffd0a77da92c23762c39b4c272754b4d481b5
Nayyer Ali <nayyer.ali@siliconplex.com>
2024-07-10 03:58:00 -0400

    starter app

---
+++ App.jsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import Navigations from './navigations';
+import {
+  View,Text
+} from 'react-native';
+
+
+function App(){
+
+  return (
+    <Navigations/>
+  );
+}
+
+export default App;
---
+++ navigations/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+    View,Text
+  } from 'react-native';
+  
+function Navigations() {
+  return (
+    <View><Text>Navigations</Text></View>
+  )
+}
+
+export default Navigations
